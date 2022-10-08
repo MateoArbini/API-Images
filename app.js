db21e0afca4d5b89f77ed58055cb89122870229b
MateoArbini <mateoarbini1998@gmail.com>
2022-10-08 20:05:35 +0000

    Api imagenes

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const multer = require("multer");
+const imageStorage = multer.diskStorage({
+    // Destination to store image     
+    destination: 'uploads', 
+      filename: (req, file, cb) => {
+          cb(null, file.fieldname + '_' + Date.now() 
+             + path.extname(file.originalname))
+            // file.fieldname is name of the field (image)
+            // path.extname get the uploaded file extension
+    }
+});
+
+const maxSize = 2 * 1024 * 1024
+
+const imageUpload = multer({
+    storage: imageStorage,
+    limits: { fileSize: maxSize },
+    fileFilter(req, file, cb) {
+     cb(undefined, true)
+  }
+}) 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post('/file', imageUpload.single('image'), (req, res) => {
+    res.send(req.file)
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+app.listen(3000, () =>{
+    console.log("andandooo")
+})
