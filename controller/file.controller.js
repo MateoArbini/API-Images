1bda0d8282f8743e7c03a6269e438b50afcf29f2
MateoArbini <mateoarbini1998@gmail.com>
2022-10-08 18:17:11 +0000

    modified route dir of resource

--- controller/file.controller.js
+++ controller/file.controller.js
@@ -37,7 +37,7 @@ const upload = async (req, res) => {
 };
 
 const getListFiles = (req, res) => {
-  const directoryPath = __basedir + "/resources/static/assets/uploads/";
+  const directoryPath = __basedir + "/resources/";
 
   fs.readdir(directoryPath, function (err, files) {
     if (err) {
